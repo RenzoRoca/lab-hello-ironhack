04ee1d041f18a4918baf6d93920e7352c0feb6ed
Renzo <renzo.roca.m@gmail.com>
2021-02-26 19:34:03 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
+    return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <nav className="navbar">
+                <div className="container">
+                    <a href="" ><img src='/images/ironhack-logo.svg' /></a>
+                    <img src='/images/menu-top.svg' />
+                </div>
+            </nav>
+            <div className="container app-intro">
+                <h1 className="col-6 text-lg fw-bold title" >Say hello to ReactJS</h1>
+                <p className="col-6">You will learn how to use the most popular frontend library and become a super Ninja developer</p>
+                <button className="btn btn-light title-button">Awesome!</button>
+            </div>
+        </header>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <img src='/images/icon1.png' />
+                    <h4 className="fw-bold text-lg">Declarative</h4>
+                    <p className="text-black-50">React makes it painless to create interactive UIs</p>
+                </div>
+                <div className="col">
+                    <img src='/images/icon2.png' />
+                    <h4 className="fw-bold text-lg">Components</h4>
+                    <p className="text-black-50">Build encapsulated components that manage their state</p>
+                </div>
+                <div className="col">
+                    <img src='/images/icon3.png' />
+                    <h4 className="fw-bold text-lg">Single-way</h4>
+                    <p className="text-black-50">A set of immutable values are passed to the components</p>
+                </div>
+                <div className="col">
+                    <img src='/images/icon4.png' />
+                    <h4 className="fw-bold text-lg" >JSX</h4>
+                    <p className="text-black-50">Statically-typed, designed to run on modern browsers</p>
+                </div>
+            </div>
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
